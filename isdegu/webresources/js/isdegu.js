a22ba438908ffea2f496ae002205de377d23ac57
Adrián <agutierrezdiaz90@gmail.com>
2022-04-27 19:30:38 +0200

    Image gallery + News section

--- isdegu/webresources/js/isdegu.js
+++ isdegu/webresources/js/isdegu.js
@@ -1,47 +1,98 @@
 // Get that hamburger menu cookin' //
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll(".navbar-burger"),
-      0
-    );
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function($el) {
-        $el.addEventListener("click", function() {
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle("is-active");
-          $target.classList.toggle("is-active");
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener("click", function () {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
+    });
+  }
+});
+
+// Smooth Anchor Scrolling
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    // console.log($target);
+
+    $trigger.addEventListener('click', (event) => {
+      itemStyle = event.target.style;
+      backImg = itemStyle.backgroundImage.slice(4, -1).replace(/"/g, "");
+      $(".is-modalImg").attr("src", backImg);
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
     }
   });
-  
-  // Smooth Anchor Scrolling
-  $(document).on("click", 'a[href^="#"]', function(event) {
-    event.preventDefault();
-    $("html, body").animate(
-      {
-        scrollTop: $($.attr(this, "href")).offset().top
-      },
-      500
-    );
-  });
-  
-  // // When the user scrolls down 20px from the top of the document, show the scroll up button
-  // window.onscroll = function() {
-  //   scrollFunction();
-  // };
-  
-  // function scrollFunction() {
-  //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  //     document.getElementById("toTop").style.display = "block";
-  //   } else {
-  //     document.getElementById("toTop").style.display = "none";
-  //   }
-  // }
+});
+
+var btn = $('#toTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
